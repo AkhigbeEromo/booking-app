59d25a23b5f33820205c2bffef15e539c5a00cd4
EROMOSELE AKHIGBE <akhigbeeromo@gmail.com>
2024-01-23 08:25:59 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	 conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are available\n", conferenceTickets,remainingTickets)
+	fmt.Println("Get your ticket here to attend")
+
+	var userName string
+	var userTickets int
+
+
+
+	
+
+}
