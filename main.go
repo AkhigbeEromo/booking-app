d86566b60263f0d278601b8812b8a392e6ef75e4
EROMOSELE AKHIGBE <akhigbeeromo@gmail.com>
2024-01-23 11:15:21 +0100

    added array component

--- main.go
+++ main.go
@@ -5,14 +5,34 @@ import "fmt"
 func main(){
 	 conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are available\n", conferenceTickets,remainingTickets)
 	fmt.Println("Get your ticket here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	var bookings = [50]string{"Nana", "Nicole", "Peter"}
+
+
+	fmt.Printf("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Printf("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Printf("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Printf("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = conferenceTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",firstName,lastName,userTickets,email )
 
 
 
